a804db53292f75cfb385129b203be54a8f9e86bd
Vitor Angelozi <vitorangelozi2013@gmail.com>
2026-07-12 15:38:31 -0400

    adding required

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,16 @@ class AuthController extends Controller
     }
 
     public function loginsubmit(Request $request){
+
+        //form validation
+        $request->validade(
+            [
+                'email' => 'required',
+                'password' => 'required'
+
+            ]
+        );
+
        echo $request->input('email');
        echo "<br>";
        echo $request->input('password');
